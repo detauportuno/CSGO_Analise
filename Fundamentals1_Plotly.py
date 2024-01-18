084cf47847c4a227f5feac4652211e64c99ff359
Andre Sampaio <89166187+detauportuno@users.noreply.github.com>
2024-01-18 01:53:35 -0300

    Update Fundamentals1_Plotly.py
    CODIGO 18.01

--- Fundamentals1_Plotly.py
+++ Fundamentals1_Plotly.py
@@ -4,16 +4,21 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 df = pd.read_csv("IRIS.csv")
-fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species", title="Using The add_trace() method With A Plotly Express Figure")
 
-fig.add_trace(
+fig = px.scatter(df, x="sepal_width", y="sepal_length", color="species",
+                  facet_col="species", title="Using The add_trace() method With A Plotly Express Figure")
+#OBS: se eu quiser que todas as infos estejam contidas no mesmo grafico, basta tirar o facet_col
+'''fig.add_trace(
     go.Scatter(
         x=[2, 4],
         y=[4, 8],
         mode="lines",
         line=go.scatter.Line(color="black"),
-        showlegend=True)
-)
+        showlegend=False)
+)'''
 
+fig = px.bar(df, x="sepal_width", y="sepal_length", color="species",
+                  facet_col="species", title="Using The add_trace() method With A Plotly Express Figure" )
 fig.show()
+
 
